dacc14ae15678eea40fda43995204e2b5b326735
Brandon Richey <brichey@teladoc.com>
2015-10-24 00:33:42 -0400

    WIP

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Reviewq.Repo
+alias Reviewq.User
+
+%User{}
+|> User.changeset(%{username: "admin", password: "test1234", password_confirmation: "test1234", admin: true})
+|> Repo.insert!
--- web/models/user.ex
+++ web/models/user.ex
@@ -4,13 +4,15 @@ defmodule Reviewq.User do
   schema "users" do
     field :username, :string
     field :password_digest, :string
-    field :score, :integer
+    field :score, :integer, default: 0
     field :admin, :boolean, default: false
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     timestamps
   end
 
-  @required_fields ~w(username password_digest score admin)
+  @required_fields ~w(username admin password password_confirmation score)
   @optional_fields ~w()
 
   @doc """
@@ -22,5 +24,15 @@ defmodule Reviewq.User do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> hash_password
+  end
+
+  defp hash_password(model) do
+    if password = get_change(model, :password) do
+      model
+      |> put_change(:password_digest, Comeonin.Bcrypt.hashpwsalt(password))
+    else
+      model
+    end
   end
 end
