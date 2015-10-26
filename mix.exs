37a6b225a98fb9fdf539a0ab488e2969ab03c848
Brandon Richey <brichey@teladoc.com>
2015-10-26 12:59:38 -0400

    Adding support for EXRM

--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule Reviewq.Mixfile do
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 1.0"}]
+     {:comeonin, "~> 1.0"},
+     {:exrm, "~> 0.19"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
